9ca49adb242fea8e77f96ae5c730188538038da8
MarcinTomkiewicz <marc.tomk@gmail.com>
2025-03-10 13:07:02 +0100

    Offers adjusted

--- src/app/common/category-list/category-list/category-list.component.ts
+++ src/app/common/category-list/category-list/category-list.component.ts
@@ -1,25 +1,33 @@
 import { Component, EventEmitter, input, Output } from '@angular/core';
 import { Category, Subcategory } from '../../../core/interfaces/i-offers';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbAccordionModule,
+  NgbDropdownModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { Subject } from 'rxjs';
 import { outputToObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [NgbAccordionModule],
+  imports: [NgbAccordionModule, NgbDropdownModule],
   templateUrl: './category-list.component.html',
-  styleUrl: './category-list.component.scss'
+  styleUrl: './category-list.component.scss',
 })
 export class CategoryListComponent {
-  readonly categories = input<Category[]>()
-  readonly subcategories = input<Subcategory[]>()
+  readonly categories = input<Category[]>();
+  readonly subcategories = input<Subcategory[]>();
   @Output() subcategorySelected = new EventEmitter<number | null>();
 
-  
+  activeCategoryId: number | null = null;
   subcategorySelected$ = outputToObservable(this.subcategorySelected);
 
   onSubcategoryClick(subcategoryId: number | null): void {
     this.subcategorySelected.next(subcategoryId);
+  }
+
+  toggleCategory(categoryId: number) {
+    this.activeCategoryId =
+      this.activeCategoryId === categoryId ? null : categoryId;
   }
 }
